ee821190954220cfd7d6d871c5ae24143526016f
WJDDesigns <wayne@wjddesigns.com>
2024-07-29 15:31:54 -0400

    Update styles.js

--- styles.js
+++ styles.js
@@ -50,36 +50,6 @@ export const styles = css`
     margin: 0;
     background-color: var(--primary-color);
     border-radius: 4px;
-    transition: width 0.5s ease-in-out;
-  }
-  .charging .progress::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-image: linear-gradient(
-      -45deg,
-      rgba(255, 255, 255, 0.2) 25%,
-      transparent 25%,
-      transparent 50%,
-      rgba(255, 255, 255, 0.2) 50%,
-      rgba(255, 255, 255, 0.2) 75%,
-      transparent 75%,
-      transparent 100%
-    );
-    background-size: 50px 50px;
-    animation: move 2s linear infinite;
-    overflow: hidden;
-  }
-  @keyframes move {
-    0% {
-      background-position: 0 0;
-    }
-    100% {
-      background-position: 50px 50px;
-    }
   }
   .level-text {
     font-size: 1.2em;
