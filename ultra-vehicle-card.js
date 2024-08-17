5ffb29de5ac67090cd7b82bb2b19d71fb6252809
Wayne Drescher <139587188+WJDDesigns@users.noreply.github.com>
2024-08-16 20:38:55 -0400

    Update ultra-vehicle-card.js

--- ultra-vehicle-card.js
+++ ultra-vehicle-card.js
@@ -3,7 +3,7 @@ import { UltraVehicleCardEditor } from "./ultra-vehicle-card-editor.js";
 import { styles } from "./styles.js";
 
 
-const version = "V1.3.1";
+const version = "V1.3.2";
 
 
 class UltraVehicleCard extends LitElement {
