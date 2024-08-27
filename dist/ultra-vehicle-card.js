4681be8f4e2fc0c9a6ca01a4276b88748a504940
Wayne Drescher <139587188+WJDDesigns@users.noreply.github.com>
2024-08-26 20:58:19 -0400

    Update ultra-vehicle-card.js

--- dist/ultra-vehicle-card.js
+++ dist/ultra-vehicle-card.js
@@ -3,8 +3,8 @@ import {
   html,
   css,
 } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
-import { version, setVersion } from "./version.js?v=8";
-setVersion("V1.6.0-beta");
+import { version, setVersion } from "./version.js?v=9";
+setVersion("V1.6.0-beta2");
 
 const sensorModule = await import("./sensors.js?v=" + version);
 const { formatEntityValue, getIconActiveState, formatBinarySensorState, isEngineOn } = sensorModule;
