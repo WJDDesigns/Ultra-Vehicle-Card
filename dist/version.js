a7af424fd253bc7f533846a5064fb90c3a0dc5dd
Wayne Drescher <139587188+WJDDesigns@users.noreply.github.com>
2025-04-01 14:54:46 -0400

    Remove old files for TypeScript rewrite

--- dist/version.js
+++
@@ -1,9 +0,0 @@
-// Upon any change to this file update 'import ... from 'version.js?v=0'' with a newer version number.
-// In all files importing this file.
-
-let version = "undefined";
-function setVersion(value) {
-  version = value;
-}
-
-export { version, setVersion };
