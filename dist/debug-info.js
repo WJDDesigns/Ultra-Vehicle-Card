afd7590696ce9eb8c2fb400b7f7fdbf82d5a273b
WJDDesigns <wayne@wjddesigns.com>
2025-04-20 07:27:48 -0400

    V2,3 - Icon Row Fix

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 2.2
-// Build Date: 2025-04-19T23:10:00.578Z
+// Version: 2.3
+// Build Date: 2025-04-20T11:24:58.278Z
 // Build Mode: production
--- dist/version.js
+++ dist/version.js
@@ -1,6 +1,6 @@
 /**
  * Ultra Vehicle Card Version
- * v2.2
+ * v2.3
  * 
  * This file is auto-generated from src/version.ts
  * DO NOT MODIFY DIRECTLY
@@ -13,6 +13,6 @@ function setVersion(value) {
 }
 
 // Set default version (will be overridden by card)
-setVersion('2.2');
+setVersion('2.3');
 
 export { version, setVersion };
