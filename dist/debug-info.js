715dbb62ef6be0350cb8c85cd9f776c315957ea3
WJDDesigns <wayne@wjddesigns.com>
2025-05-05 13:00:23 -0400

    Recommit back to stable

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 2.6.4-beta1
-// Build Date: 2025-05-05T12:28:57.103Z
+// Version: 2.6.3
+// Build Date: 2025-05-05T16:58:36.257Z
 // Build Mode: production
