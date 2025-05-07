160d71e3ee40225f2d8fa187f102658baa225318
WJDDesigns <wayne@wjddesigns.com>
2025-05-07 11:11:05 -0400

    2.6.4-beta2
    Added drag and drop in Bars and Icon Rows that will allow for arranging to swap around.
    Fixed State field in the Info Area to respect Binary Show As
    Improved Bars logic and hopefully will remove - bars in favor of - bar_x format for better yaml and uniqueness. Added migration to fix bar issues of not showing.

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 2.6.4-beta1
-// Build Date: 2025-05-07T14:01:47.506Z
+// Version: 2.6.4-beta2
+// Build Date: 2025-05-07T15:05:15.316Z
 // Build Mode: production
--- dist/version.d.ts
+++ dist/version.d.ts
@@ -2,4 +2,4 @@
  * Ultra Vehicle Card Version
  * This is the single source of truth for version information
  */
-export declare const VERSION = "2.6.4-beta1";
+export declare const VERSION = "2.6.4-beta2";
