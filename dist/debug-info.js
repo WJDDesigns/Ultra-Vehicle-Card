bf9fc3d2f614bd702e5d28cce3bf76e9db95eeb5
WJDDesigns <wayne@wjddesigns.com>
2025-05-18 09:51:19 -0400

    2.8.0-beta2
    Fix input boxes in Customize to change inputs to allow input without kicking user out.

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 2.8.0-beta1
-// Build Date: 2025-05-17T22:13:17.056Z
+// Version: 2.8.0-beta2
+// Build Date: 2025-05-18T13:45:27.149Z
 // Build Mode: production
--- dist/version.js
+++ dist/version.js
@@ -1,6 +1,6 @@
 /**
  * Ultra Vehicle Card Version
- * v2.8.0-beta1
+ * v2.8.0-beta2
  * 
  * This file is auto-generated from src/version.ts
  * DO NOT MODIFY DIRECTLY
@@ -13,6 +13,6 @@ function setVersion(value) {
 }
 
 // Set default version (will be overridden by card)
-setVersion('2.8.0-beta1');
+setVersion('2.8.0-beta2');
 
 export { version, setVersion };
