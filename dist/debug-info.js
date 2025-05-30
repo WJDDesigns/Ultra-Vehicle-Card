99c7e2bf0cae136e6b8ce47034477975b6c100a6
WJDDesigns <wayne@wjddesigns.com>
2025-05-30 10:28:20 -0400

    2.9.1-beta1
    Force icon animations to use center/center
    Possibly fix for old info migration entities

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 2.9.0
-// Build Date: 2025-05-29T18:57:00.838Z
+// Version: 2.9.1-beta1
+// Build Date: 2025-05-30T14:27:07.346Z
 // Build Mode: production
--- dist/version.d.ts
+++ dist/version.d.ts
@@ -2,4 +2,4 @@
  * Ultra Vehicle Card Version
  * This is the single source of truth for version information
  */
-export declare const VERSION = "2.9.0";
+export declare const VERSION = "2.9.1-beta1";
