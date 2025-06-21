c91a04ce229a86ecc1bcdef59c486d2af5f943a0
WJDDesigns <wayne@wjddesigns.com>
2025-06-21 10:57:56 -0400

    V3.0.0-beta7
    Fixed some things...

--- dist/debug-info.js
+++ dist/debug-info.js
@@ -1,4 +1,4 @@
 // Ultra Vehicle Card Debug Info
-// Version: 3.0.0-beta7
-// Build Date: 2025-06-16T14:53:30.790Z
+// Version: 3.0.0-beta8
+// Build Date: 2025-06-21T14:56:47.269Z
 // Build Mode: production
